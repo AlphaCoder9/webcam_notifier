fd211177902eaebdfd524915b01d7506f28e687e
Adeel.Tahir <Adeel.Tahir@cioxhealth.com>
2023-05-03 22:37:39 -0400

    Add email setup email server

--- emailing.py
+++ emailing.py
@@ -1,2 +1,28 @@
-def send_email():
-    print("Running***")
+import smtplib
+from email.message import EmailMessage
+import imghdr
+
+PASSWORD = "yujmtxanqawiolxj"
+SENDER = "adeelta302@gmail.com"
+RECEIVER = "adeelta302@gmail.com"
+
+
+def send_email(img_path):
+    email_message = EmailMessage()
+    email_message["Subject"] = "ALERT! *Movement Detected*"
+    email_message.set_content("New object found!")
+
+    with open(img_path, "rb") as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+
+    gmail = smtplib.SMTP("smtp.gmail.com", 587)
+    gmail.ehlo()
+    gmail.starttls()
+    gmail.login(SENDER, PASSWORD)
+    gmail.sendmail(SENDER, RECEIVER, email_message.as_string())
+    gmail.quit()
+
+
+if __name__ == "__main__":
+    send_email(img_path="images/image001.png")
